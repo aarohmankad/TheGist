b89ee31cf336e299e444cb3a0f986eb14aba5185
Aaroh <aaroh@ubuntu.(none)>
2014-02-23 21:23:55 -0800

    Initial Commit.

---
+++ js/news.js
@@ -0,0 +1,37 @@
+
+function callback(data) 
+{
+
+	// Length is 0-24
+
+	var links = data.results.links;
+	console.log(links);
+
+	for (var i = 0; i < links.length; i++) {
+		
+		// Header
+		$(".row" + (i+1)).find("#article-name").html(links[i].title.text);
+
+		// Link to Reddit Article
+		$(".row" + (i+1)).find("#article-link").attr("href", links[i].title.href);
+
+		// Submitters Profile
+		$(".row" + (i+1)).find("#username").html("by " + links[i].submitter.text);
+
+		// Link to submitters profile
+		$(".row" + (i+1)).find("#username-link").attr("href", links[i].submitter.href);
+
+		// Show number of comments
+		$(".row" + (i+1)).find("#comment-num").html(links[i].numComments.text);
+
+		// Link to comments
+		$(".row" + (i+1)).find("#username-link").attr("href", links[i].numComments.href);		
+	};
+}
+
+$.ajax
+({
+	"url":"http://www.kimonolabs.com/api/42h5m2oy?apikey=73388f3a9262f1c93b0116ffed06c96a&callback=callback",
+	"crossDomain":true,
+	"dataType":"jsonp"
+});
