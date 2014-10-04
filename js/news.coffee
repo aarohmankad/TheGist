6a6cb56660c7a74f7dcc21c493cc75aa0970facf
aarohmankad <aarohmankad@gmail.com>
2014-10-03 17:03:31 -0700

    Now summarizes articles on hover

--- js/news.coffee
+++ js/news.coffee
@@ -9,6 +9,16 @@ app.directive 'newsList', ->
 	templateUrl: '../views/news-list.html'
 	controller: ($http, $scope) ->
 		$http.get('http://www.reddit.com/r/news.json').success (resp_data) ->
-			$scope.news = resp_data.data.children;
+			$scope.news = resp_data.data.children
 			return
+
+		$scope.showSummary = (article) ->
+			if(!article.data.summary)
+				$http.get('http://cors-anywhere.herokuapp.com/http://clipped.me/algorithm/clippedapi.php?url=' + article.data.url).success (resp) ->
+					article.data.summary = ""
+					for i in resp.summary
+						article.data.summary += " " + i
+					console.log article.data.summary
+					return
+				return
 		return
--- js/news.js
+++ js/news.js
@@ -14,6 +14,20 @@
         $http.get('http://www.reddit.com/r/news.json').success(function(resp_data) {
           $scope.news = resp_data.data.children;
         });
+        $scope.showSummary = function(article) {
+          if (!article.data.summary) {
+            $http.get('http://cors-anywhere.herokuapp.com/http://clipped.me/algorithm/clippedapi.php?url=' + article.data.url).success(function(resp) {
+              var i, _i, _len, _ref;
+              article.data.summary = "";
+              _ref = resp.summary;
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                i = _ref[_i];
+                article.data.summary += " " + i;
+              }
+              console.log(article.data.summary);
+            });
+          }
+        };
       }
     };
   });
